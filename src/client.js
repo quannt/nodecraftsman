8d323177a5a48c04578c13ba9435f0c320f019e6
quannt <simpleplan.vn@gmail.com>
2016-01-18 22:33:32 +0800

    add example of server client with async

---
+++ src/client.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var request = require('request');
+var async = require('async');
+
+var name, status;
+
+var getUsername = function(callback){
+	request.get('http://localhost:8080/getUserName?id=1234',
+	function(err, res, body){
+		var result = JSON.parse(body);
+		name = result.value;
+		callback(err, result.value);
+	});
+};
+
+var getUserStatus = function(callback){
+	request.get(
+		'http://localhost:8080/getUserStatus?id=1234',
+		function(err, res, body){
+		var result = JSON.parse(body);
+		status = result.value;
+		callback(err, result.value);
+	});
+};
+
+async.parallel([getUsername, getUserStatus], function (err, results){
+	console.log('The status of user', results[0], 'is', results[1]);
+});
